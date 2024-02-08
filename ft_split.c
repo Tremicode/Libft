097efcf7b3e5df58bdf1f508fb9643957a6ea4e9
Tremicode <23tremi@gmail.com>
2024-02-08 12:21:55 +0100

    split correcion

--- ft_split.c
+++ ft_split.c
@@ -6,48 +6,62 @@
 /*   By: ctremino <ctremino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 18:11:42 by ctremino          #+#    #+#             */
-/*   Updated: 2024/02/04 18:54:55 by ctremino         ###   ########.fr       */
+/*   Updated: 2024/02/08 12:20:03 by ctremino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int 	ft_wordlen(char const *s, char c) /*fun auxiliar calcula long de una palabra */
+static int	ft_wordlen(char const *s, char c)
+		/*fun auxiliar calcula long de una palabra */
 {
-	int len  = 0;
+	int len = 0;
 	while (*s && *s != c)
 	{
 		len++;
 		s++;
-	}	
+	}
 	return (len);
 }
-static int	ft_countwords(char const *s, char c) /* fun aux para contar palabras en la cadena*/
+static int	ft_countwords(char const *s, char c)
+		/* fun aux para contar palabras en la cadena*/
 {
 	int count = 0;
 	while (*s)
 	{
-		if  (*s != c)
+		if (*s != c)
 		{
 			count++;
-			s += ft_wordlen(s,c);
+			s += ft_wordlen(s, c);
 		}
 		else
 			s++;
 	}
 	return (count);
 }
-char	**ft_split(char const *s, char c) /*funcion principal para dividir la cadena en palabras*/
+static void	free_strs(char **strs) /* funcion añadida para liberar memoria*/
 {
-	char	**strs;
-	int		i;
-	int 	count;
+	int i = 0;
+	while (strs[i])
+		
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs); /*liberia memoria asignada en el arreglo*/
+}
+char	**ft_split(char const *s, char c)
+		/*funcion principal para dividir la cadena en palabras*/
+{
+	char **strs;
+	int i;
+	int count;
 
 	if (!s) /*verifica si la cadena de entrada es nula*/
 		return (NULL);
 
-	count = ft_countwords(s,c);
-	strs = malloc(sizeof(char  *) *(count + 1));
+	count = ft_countwords(s, c);
+	strs = malloc(sizeof(char *) * (count + 1));
 
 	if (!strs)
 		return (NULL);
@@ -57,10 +71,15 @@ char	**ft_split(char const *s, char c) /*funcion principal para dividir la caden
 	{
 		if (*s != c)
 		{
-			strs[i] = ft_substr(s, 0, ft_wordlen(s, c)); /* extraer subcadema y almacenar en el arreglo*/
-			if (!strs[i++])
-				return (free_strs(strs));
-			s += ft_wordlen (s,c);
+			strs[i] = ft_substr(s, 0, ft_wordlen(s, c));
+				/* extraer subcadema y almacenar en el arreglo*/
+			if (!strs[i])
+			{
+				free_strs(strs);
+				return (NULL);
+			}
+			s += ft_wordlen(s, c);
+			i++;
 		}
 		else
 			s++;
