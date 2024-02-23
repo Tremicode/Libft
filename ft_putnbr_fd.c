3d4adb2f639033d504ba9b9661cd8c83c0d0d519
Carlos Tremino Fernandez <ctremino@c1r10s3.42madrid.com>
2024-02-23 15:26:31 +0100

    fd2

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ctremino <ctremino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:45:05 by ctremino          #+#    #+#             */
-/*   Updated: 2024/02/19 13:46:34 by ctremino         ###   ########.fr       */
+/*   Updated: 2024/02/23 15:24:46 by ctremino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,3 +30,27 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	ft_putchar_fd(n % 10 + '0', fd);
 }
+
+/*#include <fcntl.h> //  constantes O_WRONLY y O_CREAT
+
+int main() {
+    int fd; // 
+    int numero = 12345; 
+
+    //  archivo "output.txt" en modo escritura, si no existe lo crea
+    fd = open("output.txt", O_WRONLY | O_CREAT, 0644);
+
+    // Verificamos si la apertura del archivo fue exitosa
+    if (fd == -1) {
+        ft_putstr_fd("Error al abrir el archivo\n", 2); // 
+        return 1; // Salimos del programa con un código de error
+    }
+
+    // Llamamos a nuestra función para imprimir el número en el archivo
+    ft_putnbr_fd(numero, fd);
+
+    // Cerramos el archivo
+    close(fd);
+
+    return 0; // Salimos del programa con éxito
+}*/
