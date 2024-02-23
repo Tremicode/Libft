5b3a93249aa4c2ee324dc52780c9fef57b485d9f
Carlos Tremino Fernandez <ctremino@c1r10s3.42madrid.com>
2024-02-23 15:14:34 +0100

    fd main

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: ctremino <ctremino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:35:11 by ctremino          #+#    #+#             */
-/*   Updated: 2024/02/19 13:42:30 by ctremino         ###   ########.fr       */
+/*   Updated: 2024/02/23 14:49:22 by ctremino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,27 @@ void	ft_putendl_fd(char *s, int fd)
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
+/*#include <fcntl.h> // Para las constantes O_WRONLY y O_CREAT
+
+
+int main() {
+    int fd; // Descriptor de archivo
+    char *cadena = "¡Hola, mundo estoy probando un fd!";
+
+    // Abrimos un archivo "salida.txt" en modo de escritura.
+    fd = open("salida.txt", O_WRONLY | O_CREAT, 0644);
+
+    // Verificamos si la apertura del archivo fue exitosa
+    if (fd == -1) {
+        ft_putendl_fd("Error al abrir el archivo", 2); // Imprimimos mensj error
+        return 1; // Salimos del programa con un código de error
+    }
+
+    // Llamamos a nuestra función para imprimir la cadena en el archivo
+    ft_putendl_fd(cadena, fd);
+
+    // Cerramos el archivo
+    close(fd);
+
+    return 0; // Salimos del programa con éxito
+}*/
